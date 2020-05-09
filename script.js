8ce0ce2f24847856c457c5b5817004551ce59bd6
Undiess <nguyen.andy67@gmail.com>
2020-05-09 21:35:32 +0930

    asd

---
+++ script.js
@@ -0,0 +1,5 @@
+const inquirer = require("inquirer")
+const intern = require("./lib/Intern.js")
+const manager = require("./lib/Manager.js")
+const employee = requre("./lib/Employee.js")
+const engineer = require("./lib/Engineer.js")
